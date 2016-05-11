d0430ef23c305979bb93f25b0570a6a437c5b238
iulia0103 <iulia.leancu@gmail.com>
2016-05-11 13:35:02 +0200

    testing the models with Rspec-relationships, validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:likes) }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:message) }
+  end
 end
