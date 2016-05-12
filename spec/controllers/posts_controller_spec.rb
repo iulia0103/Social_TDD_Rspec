8f8f5058df96ca0975bb3b69cb6a03d521595308
iulia0103 <iulia.leancu@gmail.com>
2016-05-12 11:04:33 +0200

    fix the posts controller specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,35 +1,146 @@
 require 'rails_helper'
 
-RSpec.describe "posts/index", type: :view do
-  let(:user) { User.create(email: "user@social.com", password: "12345678") }
-  let(:other_user) { User.create(email: "other-user@social.com", password: "12345678") }
+RSpec.describe PostsController, type: :controller do
 
-  before(:each) do
-    assign(:posts, [
-      Post.create!(
-        message: "Hello, I'm writing tests!",
-        user: user
-      ),
-      Post.create!(
-        message: "Hi, Here's another post!",
-        user: other_user
-      )
-    ])
+  let(:user) { User.create(email: "poster@social.com", password: "12345678") }
 
-    render
+  let(:valid_attributes) do
+    {
+      message: "I'm writing tests now!",
+      user: user
+    }
   end
 
-  it "renders the title of the page" do
-    assert_select "h1", text: "Posts", count: 1
+  let(:invalid_attributes) do
+    {
+      message: ""
+    }
   end
 
-  it "renders a list of posts" do
-    assert_select ".post>p", text: "Hello, I'm writing tests!", count: 1
-    assert_select ".post>p", text: "Hi, Here's another post!", count: 1
+  let!(:a_post) { Post.create! valid_attributes }
+
+  describe "GET #index" do
+    it "assigns all posts as @posts" do
+      get :index
+      expect(assigns(:posts)).to eq([a_post])
+    end
+
+    context "when logged in" do
+      login_user
+    end
   end
 
-  it "shows the user's email with the post" do
-    assert_select ".post>.meta", text: user.email, count: 1
-    assert_select ".post>.meta", text: other_user.email, count: 1
+  describe "GET #show" do
+    it "assigns a post as @post" do
+      get :show, {id: a_post.id}
+      expect(assigns(:post)).to eq(a_post)
+    end
+
+    context "when logged in" do
+      login_user
+    end
   end
+
+  describe "GET #new" do
+    it "assigns a new post as @post" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    context "when logged in" do
+      login_user
+
+        it " assigns a new post as @post" do
+          get :new
+          expect(assigns(:post)).to be_a_new(Post)
+        end
+
+        it "renders the new template" do
+          get :new
+          expect(response).to render_template("new")
+        end
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested post as @post" do
+      get :edit, {id: a_post.to_param}
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    context "when logged in" do
+      login_user
+
+        it " assigns the requested post as @post" do
+          get :edit, {id: a_post.to_param}
+          expect(assigns(:post)).to eq(a_post)
+        end
+
+        it "renders the edit template" do
+          get :edit, {id: a_post.to_param}
+          expect(response).to render_template("edit")
+        end
+    end
+  end
+
+
+  describe "POST #create" do
+     it "is not accessible when not logged in" do
+       post :create, {post: valid_attributes}
+       expect(response).to redirect_to new_user_session_path
+     end
+
+    context "when logged in" do
+      login_user
+
+      context "with valid params" do
+       it "creates a new Post" do
+         expect {
+           post :create, {post: valid_attributes}
+         }.to change(Post, :count).by(1)
+       end
+
+       it "assigns a newly created post as @post" do
+         post :create, {post: valid_attributes}
+         expect(assigns(:post)).to be_a(Post)
+         expect(assigns(:post)).to be_persisted
+       end
+
+       it "assigns the logged in user as the post's creater" do
+         post :create, {post: valid_attributes}
+         expect(assigns(:post).user).to eq user
+       end
+     end
+
+     context "with invalid params" do
+       it "assigns a newly created but unsaved post as @post" do
+         post :create, {post: invalid_attributes}
+         expect(assigns(:post)).to be_a_new(Post)
+       end
+
+       it "re-renders the 'new' template" do
+          post :create, {post: invalid_attributes}
+          expect(response).to redirect_to new_post_path
+        end
+     end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "is not accessible when not logged in" do
+      delete :destroy, id: a_post.id
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    context "when logged in" do
+      login_user
+
+      it "destroys an existing Post" do
+        expect {
+          delete :show, id: a_post.id
+        }.to change(Post, :count).by(0)
+      end
+    end
+  end
+
 end
